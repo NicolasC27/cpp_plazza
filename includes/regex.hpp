5f8481d612bcff986bbbcb606ab28734c03ee0a1
Lucas DEBOUTÉ <P147x.x@gmail.com>
2017-04-15 11:43:16 +0200

    ip_tracker for regex ready

---
+++ includes/regex.hpp
@@ -0,0 +1,27 @@
+//
+// regex.cpp for plazza in /home/codeine/Projects/B4/CPP/cpp_plazza/includes
+// 
+// Made by Lucas Debouté
+// Login   <lucas.deboute@epitech.eu>
+// 
+// Started on  Thu Apr 13 19:34:42 2017 Lucas Debouté
+// Last update Fri Apr 14 12:54:16 2017 Lucas Debouté
+//
+
+#ifndef REGEX_HPP_
+# define REGEX_HPP_
+
+# include <vector>
+# include <string>
+# include <regex>
+# include <iostream>
+
+class regex
+{
+public:
+  regex(){}
+  ~regex(){}
+  std::vector<std::string> ip_tracker(const std::string &toFind);
+};
+
+#endif /* REGEX_HPP_ */
---
+++ srcs/regex.cpp
@@ -0,0 +1,33 @@
+//
+// regex.cpp for plazza in /home/codeine/Projects/B4/CPP/cpp_plazza/srcs
+// 
+// Made by Lucas Debouté
+// Login   <lucas.deboute@epitech.eu>
+// 
+// Started on  Thu Apr 13 19:37:37 2017 Lucas Debouté
+// Last update Sat Apr 15 11:42:31 2017 Lucas Debouté
+//
+
+#include "regex.hpp"
+
+std::vector<std::string>	regex::ip_tracker(const std::string &toFind)
+{
+  std::smatch matches;
+  std::regex ip_combinaison("(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)");
+  std::vector<std::string>	tmp;
+
+  for (std::sregex_iterator it = std::sregex_iterator(toFind.begin(), toFind.end(), ip_combinaison);
+       it != std::sregex_iterator(); it++)
+    {
+      std::smatch m = *it;
+      tmp.push_back(m[0]);
+    }
+  return tmp;
+}
+
+int	main()
+{
+  regex	wee;
+
+  wee.ip_tracker("192.168.1.1 1.1.1.1");  
+}
